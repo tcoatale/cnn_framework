744acb1680c1c0f379a0b1a8badea444ecf6a418
TnG <tanguy.coatalem@gmail.com>
2016-06-07 00:37:41 +0100

    Unicity of names

--- submission.py
+++ submission.py
@@ -16,35 +16,34 @@ application = application_interface.get_application()
 
 #%%
 def submission():
-  with tf.Graph().as_default() as g:
-    images, labels = model.inputs('submission') 
-    logits = model.inference(images)
-    
-    # Restore the moving average version of the learned variables for eval.
-    variable_averages = tf.train.ExponentialMovingAverage(application.moving_average_decay)
-    variables_to_restore = variable_averages.variables_to_restore()
-    saver = tf.train.Saver(variables_to_restore)
-    
-    with tf.Session() as sess:
-      ckpt = tf.train.get_checkpoint_state(application.ckpt_dir)
-    if ckpt and ckpt.model_checkpoint_path:
-      # Restores from checkpoint
-      saver.restore(sess, ckpt.model_checkpoint_path)
-    else:
-      print('No checkpoint file found')
-      return
+  images, labels = model.inputs('submission') 
+  logits = model.inference(images)
 
-    num_iter = int(math.ceil(application.num_examples / application.batch_size))
-    step = 0
-    
-    while step < num_iter:
-      labels = sess.run([labels])
-      logits = sess.run([logits])
-    
-      print(labels[0])
-      print(logits[0])
-      step += 1
-      break
+  # Restore the moving average version of the learned variables for eval.
+  variable_averages = tf.train.ExponentialMovingAverage(application.moving_average_decay)
+  variables_to_restore = variable_averages.variables_to_restore()
+  saver = tf.train.Saver(variables_to_restore)
+
+  with tf.Session() as sess:
+    ckpt = tf.train.get_checkpoint_state(application.ckpt_dir)
+  if ckpt and ckpt.model_checkpoint_path:
+    # Restores from checkpoint
+    saver.restore(sess, ckpt.model_checkpoint_path)
+  else:
+    print('No checkpoint file found')
+  return
+
+  num_iter = int(math.ceil(application.num_examples / application.batch_size))
+  step = 0
+
+  while step < num_iter:
+    labels_loc = sess.run([labels])
+    logits_loc = sess.run([logits])
+
+    print(labels_loc[0])
+    print(logits_loc[0])
+    step += 1
+    break
 
 
 def main(argv=None):
