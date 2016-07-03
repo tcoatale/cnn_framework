8e1e2fa3ed007072f9bf3206e6ec84ed55ab6f87
TnG <tanguy.coatalem@gmail.com>
2016-07-03 20:24:22 +0100

    Fix: Removed it. Unused

--- configurations/losses/driver_augmented.py
+++ configurations/losses/driver_augmented.py
@@ -4,8 +4,8 @@ def training_loss(dataset, logits, labels):
   logits1, true_logits = logits
   labels1, true_labels = dataset.split_labels(labels)
 
-  training_loss = evaluation_loss(dataset, true_logits, true_labels)
+  training_loss = evaluation_loss(true_logits, true_labels)
   return training_loss
   
-def evaluation_loss(dataset, logits, labels):
+def evaluation_loss(logits, labels):
   return cross_entropy(logits, labels)
