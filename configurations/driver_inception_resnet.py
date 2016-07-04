5f893cdb28e64a79ce12609a11c2458cb6adb572
Tanguy Coatalem <tanguy.coatalem15@imperial.ac.uk>
2016-07-04 16:17:09 +0100

    Updated models

---
+++ configurations/driver_inception_resnet.py
@@ -0,0 +1,27 @@
+from configurations.helper import initialize_directories
+import configurations.datasets.driver_augmented as dataset
+import configurations.training.config1 as training_params
+import configurations.losses.driver_augmented as loss
+import configurations.models.inception_resnet as model
+
+name = 'inception_resnet'
+log_dir = 'log'
+ckpt_dir = 'ckpt'
+eval_dir = 'eval'
+
+log_dir, ckpt_dir, eval_dir = initialize_directories([log_dir, ckpt_dir, eval_dir], dataset.name, name)
+
+def inference(input, testing=False):
+    return None, model.inference(input, training_params.keep_prob, dataset, testing)
+    
+def training_loss(logits, labels):
+    return loss.training_loss(dataset, logits, labels)
+    
+def evaluation_loss(logits, labels):
+    return loss.evaluation_loss(dataset, logits, labels)
+    
+#%%
+display_freq = 10
+eval_freq = 300
+summary_freq = 50
+save_freq = 1000
