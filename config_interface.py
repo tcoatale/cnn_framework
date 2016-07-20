cd5b97bdabdf5a788f2d7d98921f0be54d6bd89a
Neurobeast <neurobeast>
2016-07-20 18:24:07 +0100

    Updated models

--- config_interface.py
+++ config_interface.py
@@ -1,26 +1,26 @@
 import configurations.pn_tiny_vgg
 import configurations.pn_tiny_alex
 import configurations.pn_normal_alex_32
-import configurations.pn_normal_alex_64
-import configurations.pn_tiny_resnet
+import configurations.pn_small_alex_64
+import configurations.pn_tiny_resnet_32
 import configurations.pn_small_resnet
 
 import configurations.driver_tiny_vgg
 import configurations.driver_tiny_alex
-import configurations.driver_normal_alex_32
+import configurations.driver_small_alex_32
 import configurations.driver_normal_alex_64
 
 configurations_dictionnary = {'pn' : {
                                 'tiny_vgg': configurations.pn_tiny_vgg, 
                                 'tiny_alex': configurations.pn_tiny_alex,
-                                'tiny_resnet': configurations.pn_tiny_resnet,
+                                'tiny_resnet_32': configurations.pn_tiny_resnet_32,
                                 'small_resnet': configurations.pn_small_resnet,
                                 'normal_alex_32': configurations.pn_normal_alex_32,
-                                'normal_alex_64': configurations.pn_normal_alex_64},
+                                'small_alex_64': configurations.pn_small_alex_64},
                               'driver' : {
                                 'tiny_vgg': configurations.driver_tiny_vgg, 
                                 'tiny_alex': configurations.driver_tiny_alex,
-                                'normal_alex_32': configurations.driver_normal_alex_32,
+                                'small_alex_32': configurations.driver_small_alex_32,
                                 'normal_alex_64': configurations.driver_normal_alex_64},
                              }
 
--- configurations/pn_tiny_resnet_32.py
+++ configurations/pn_tiny_resnet_32.py
@@ -1,12 +1,12 @@
 from configurations.configuration import Configuration
 
-import configurations.datasets.pn.t_64 as dataset
+import configurations.datasets.pn.t_32 as dataset
 import configurations.training.config2 as training_params
 import configurations.losses.pn as loss
 import configurations.models.resnet.tiny as model
 
 def get_config():
-  name = 'tiny_resnet'
+  name = 'tiny_resnet_32'
   freqs = {'display': 10, 'summary': 50, 'save': 500}
   config = Configuration(name, dataset, training_params, loss, model, freqs)
   return config
