b450774598b14bd724d669fd69f492cb81d81d0d
Tanguy Coatalem <tanguy.coatalem15@imperial.ac.uk>
2016-07-20 18:45:43 +0100

    Modifi8ed small resnet

--- configurations/models/resnet/small.py
+++ configurations/models/resnet/small.py
@@ -1,31 +1,19 @@
-from configurations.models.blocks.layers import conv2d_layer, pool_layer
-from configurations.models.blocks.architecture_blocks import resnet_inception_block
-from configurations.models.blocks.output_blocks import fc_output
+from configurations.models.blocks.architecture_blocks import resnet_starter
+from configurations.models.blocks.architecture_blocks import resnet_macro_block as block
+from configurations.models.blocks.output_blocks import fc_output, average_pool_vector
 
 #%%
 def architecture(input):
   print(input.get_shape())
-  conv0 = conv2d_layer(input, [11, 11], 96, name='conv0')
-
-  block1 = resnet_inception_block(conv0, 'block1')
-  block2 = resnet_inception_block(block1, 'block2')
-  pool3 = pool_layer(block2, 2, name='pool3')
-  
-  block4 = resnet_inception_block(pool3, 'block4')
-  block5 = resnet_inception_block(block4, 'block5')
-  block6 = resnet_inception_block(block5, 'block6')
-  block7 = resnet_inception_block(block6, 'block7')
-  pool8 = pool_layer(block7, 2, name='pool8')
-  
-  block9 = resnet_inception_block(pool8, 'block9')
-  block10 = resnet_inception_block(block9, 'block10')
-  block11 = resnet_inception_block(block10, 'block11')
-  block12 = resnet_inception_block(block11, 'block12')
-  block13 = resnet_inception_block(block12, 'block13')
-  out_conv = pool_layer(block13, 2, name='out_conv')
-  
-  print(out_conv.get_shape())
-  return out_conv
+  start = resnet_starter(input, 64)
+  macro_b1 = block(start, channels = 128, name='macro_b1')
+  macro_b2 = block(macro_b1, channels = 256, name='macro_b2')
+  macro_b3 = block(macro_b2, channels = 384, name='macro_b3')
+  macro_b4 = block(macro_b3, channels = 256, name='macro_b4')
+  print(macro_b4.get_shape())
+  return macro_b4
   
 def output(input, dataset):
-  return fc_output(input, dataset, [1024, 1024])
+  classes = dataset.classes
+  reduction = average_pool_vector([3, 3], classes ** 2, input, name='pool_reduce_out')
+  return fc_output(reduction, dataset, [384, 192])
