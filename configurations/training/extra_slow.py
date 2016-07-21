8b2cd86b7327905857684bef081aaf4354db29d8
Tanguy Coatalem <tanguy.coatalem15@imperial.ac.uk>
2016-07-21 16:32:30 +0100

    Added lower learning rate config

---
+++ configurations/training/extra_slow.py
@@ -0,0 +1,12 @@
+#%% Training information
+batch_size = 256
+max_steps = 60000
+
+#%%
+moving_average_decay=0.9999
+num_epochs_per_decay=50.0
+learning_rate_decay_factor=1e-4
+initial_learning_rate=0.1
+
+#%%
+keep_prob = 0.8
