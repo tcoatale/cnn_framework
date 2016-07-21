eb474360b687504490abe180a081d60ab3791614
Tanguy Coatalem <tanguy.coatalem15@imperial.ac.uk>
2016-07-21 16:34:57 +0100

    Registered new training params in interface

--- configurations/interfaces/training_interface.py
+++ configurations/interfaces/training_interface.py
@@ -3,7 +3,7 @@ import configurations.training.fast
 import configurations.training.slow
 import configurations.training.slower
 
-params_dictionnary = {'fast': configurations.training.fast, 'slow' :configurations.training.slow, 'slower': configurations.training.slower}
+params_dictionnary = {'fast': configurations.training.fast, 'slow' :configurations.training.slow, 'slower': configurations.training.slower, 'extra_slow': configurations.training.extra_slow}
 
 def get_params(name):
   try:
