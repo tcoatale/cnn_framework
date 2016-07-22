384b71bbb87a4a48ad4ed393c8d3d0f754bc041c
Tanguy Coatalem <tanguy.coatalem15@imperial.ac.uk>
2016-07-22 11:41:47 +0100

    Readapted datasets 128 and 96 of driver to new standards

--- configurations/datasets/driver/t_128.py
+++ configurations/datasets/driver/t_128.py
@@ -1,39 +1,21 @@
 import os
 import tensorflow as tf
 from functools import reduce
+from configurations.datasets.driver.driver import *
 
-name = 'driver'
 size = 128
+original_shape=[size, size, 3]
 data_dir = os.path.join('data', 'processed', name, str(size))
 
-#%% Dataset information
-train_size=14000
-valid_size=8000
-submission_size = 80000
-label_bytes=2
-id_bytes = 4
-original_shape=[size, size, 3]
-classes=10
-sub_classes=2
-
-#%%
-classes_1=2
-classes=10
-imsize=96
+imsize=100
 imshape=[imsize, imsize, 3]
 n_input = reduce(int.__mul__, imshape)
 
-def distort_inputs(reshaped_image):
-  distorted_image = tf.random_crop(reshaped_image, [imsize, imsize, 3])
-  distorted_image = tf.image.random_brightness(distorted_image, max_delta=200)
-  distorted_image = tf.image.random_contrast(distorted_image, lower=0.2, upper=3.8)
+
+#%%
+def distort_inputs(distorted_image):
+  distorted_image = tf.random_crop(distorted_image, imshape)
+  distorted_image = tf.image.random_brightness(distorted_image, max_delta=65)
+  distorted_image = tf.image.random_contrast(distorted_image, lower=0.20, upper=1.80)
   float_image = tf.image.per_image_whitening(distorted_image)
-  
   return float_image
-
-
-def split_labels(original_label):
-  label2 = tf.cast(tf.div(original_label, 256), tf.int32)
-  label1 = tf.sub(original_label, tf.mul(label2, 256))
-  
-  return label1, label2
