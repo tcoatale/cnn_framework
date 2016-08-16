fe3f4e157192db7da7775677b937458c17c2fd2f
Tanguy Coatalem <tanguy.coatalem15@imperial.ac.uk>
2016-08-16 12:28:21 +0100

    Updated naiming for simple loss

--- configurations/losses/simple.py
+++ configurations/losses/simple.py
@@ -2,11 +2,11 @@
 from configurations.losses.loss_functions import classification_rate, sparse_cross_entropy
 
 def training_loss(dataset, logits, labels):
-  loss = sparse_cross_entropy(logits, labels, dataset.classes, name='training_loss')
+  loss = sparse_cross_entropy(logits, labels, dataset.classes, name='loss_training')
   return loss
   
 def evaluation_loss(dataset, logits, labels):  
-  loss = sparse_cross_entropy(logits, labels, dataset.classes, name='evaluation_loss')
+  loss = sparse_cross_entropy(logits, labels, dataset.classes, name='loss_eval')
   return loss
   
 def classirate(dataset, logits, labels):
