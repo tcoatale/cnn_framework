b69db72a6b94c284ce866f62ed6d4d9ded9ff170
Tanguy Coatalem <tanguy.coatalem15@imperial.ac.uk>
2016-08-22 13:28:28 +0100

    Updated resnet architecture

--- configurations/models/resnet/tiny.py
+++ configurations/models/resnet/tiny.py
@@ -3,7 +3,7 @@ from configurations.models.blocks.architecture_blocks import resnet_macro_block 
 from configurations.models.blocks.output_blocks import fc_output, average_pool_vector
 
 #%%
-def architecture(input):
+def architecture(input, add_filters):
   print(input.get_shape())
   start = resnet_starter(input, 64)
   macro_b1 = block(start, channels = 128, name='macro_b1')
@@ -11,7 +11,7 @@ def architecture(input):
   print(macro_b2.get_shape())
   return macro_b2
   
-def output(input, dataset):
+def output(input, features, dataset):
   classes = dataset.classes
   reduction = average_pool_vector([3, 3], classes ** 2, input, name='pool_reduce_out')
   return fc_output(reduction, dataset.classes, [192])
