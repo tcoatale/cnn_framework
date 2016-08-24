2ee76fc4932857d19dc5b241308a15968aa1b83b
Neurobeast <neurobeast>
2016-08-24 14:14:07 +0100

    Updated training params

--- configurations/training/slow.py
+++ configurations/training/slow.py
@@ -6,7 +6,7 @@ max_steps = 400000
 moving_average_decay = 0.9999
 num_epochs_per_decay = 1000.0
 learning_rate_decay_factor = 0.1
-initial_learning_rate = 1e-1
+initial_learning_rate = 5e-2
 
 #%%
 keep_prob = 0.50
