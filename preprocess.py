5a06e21522ac58914ef077ff76261a5e38cce185
Tanguy Coatalem <tanguy.coatalem15@imperial.ac.uk>
2016-08-29 16:20:11 +0100

    Updated batch file sizes

--- preprocess.py
+++ preprocess.py
@@ -3,7 +3,7 @@ from preprocessing.interfaces.dataset_interface import get_dataset
 from preprocessing.managers import preprocessing_manager
 
 resize = [64, 64]
-batch_size = 5000
+batch_size = 1000
 
 dataset = get_dataset('pcle')
 preprocessing_manager = preprocessing_manager.PreprocessingManager(dataset=dataset, 
