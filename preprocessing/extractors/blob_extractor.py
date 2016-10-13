cf1e7d2bbd83255d760ce098dfec162b8e313a1f
Tanguy Coatalem <tanguy.coatalem@gmail.com>
2016-10-13 11:17:08 +0200

    Updated Blob Extractor to create two images

--- preprocessing/extractors/blob_extractor.py
+++ preprocessing/extractors/blob_extractor.py
@@ -1,13 +1,59 @@
 import skimage.io
 import skimage.color
 import os
-from skimage.feature import blob_dog, blob_log, blob_doh
-from math import sqrt
 import numpy as np
 from functools import reduce
 from datetime import datetime
 import pathos.multiprocessing as mp
+import skimage.exposure
 import glob
+import cv2
+
+
+def white_params():
+  params = cv2.SimpleBlobDetector_Params()
+
+  params.blobColor = 255
+  
+  params.minThreshold = 0
+  params.maxThreshold = 500
+  
+  params.minArea = 40
+  params.maxArea = 50000
+  params.filterByArea = True
+   
+  params.minCircularity = 0.50
+  params.filterByCircularity = True
+   
+  params.minConvexity = 0.50
+  params.filterByConvexity = True
+   
+  params.filterByInertia = True
+  params.minInertiaRatio = 0.3
+  
+  return params
+
+def black_params():
+  params = cv2.SimpleBlobDetector_Params()
+
+  params.blobColor = 0
+  
+  params.minThreshold = 0
+  params.maxThreshold = 100
+  
+  params.minArea = 400
+  params.maxArea = 50000
+  params.filterByArea = True
+   
+  params.minCircularity = 0.002
+  params.filterByCircularity = True
+   
+  params.minConvexity = 0.02
+  params.filterByConvexity = True
+   
+  params.filterByInertia = True
+  params.minInertiaRatio = 0.003  
+  return params  
 
 class BlobFeatureExtractor:
   def is_coorect_coord(self, coord, image_shape)  :
@@ -26,7 +72,9 @@ class BlobFeatureExtractor:
 
 
   def get_coordinates(self, blob):
-    x, y, r = blob
+    x, y = blob.pt
+    r = blob.size
+
     center = np.array([x, y], dtype=np.int16)
     x_min, y_min = center - int(r)
     x_max, y_max = center + int(r)
@@ -41,17 +89,13 @@ class BlobFeatureExtractor:
     return coords_blob
   
   
-  def get_blobs(self, image):
-    blobs_log = blob_log(image, max_sigma=30, num_sigma=10, threshold=.1)
-    blobs_log[:, 2] = blobs_log[:, 2] * sqrt(2)
-    blobs_dog = blob_dog(image, max_sigma=30, threshold=.1)
-    blobs_dog[:, 2] = blobs_dog[:, 2] * sqrt(2)
-    blobs_doh = blob_doh(image, max_sigma=30, threshold=.01)
-    
-    all_blobs = np.vstack([blobs_log, blobs_doh, blobs_dog])
-    all_blobs = filter(lambda b: b[2] > 4, all_blobs)
-    all_blobs = list(filter(lambda b: b[2] < 60, all_blobs))
-    return all_blobs
+  def get_blobs(self, image, params):
+    detector = cv2.SimpleBlobDetector_create(params)
+    keypoints = detector.detect(image)
+       
+    keypoints = filter(lambda b: b.size > 3, keypoints)
+    keypoints = list(filter(lambda b: b.size < 100, keypoints))
+    return keypoints
   
   
   def update_blobs(self, image, blob):
@@ -60,9 +104,8 @@ class BlobFeatureExtractor:
     coords_blob = list(zip(*coords_blob))
     image[coords_blob] = 1  
   
-  def extract(self, file):
-    image = skimage.io.imread(file)
-    blobs = self.get_blobs(image)
+  def extract(self, image, params):
+    blobs = self.get_blobs(image, params)
     blob_image = np.zeros(image.shape)
     list(map(lambda b: self.update_blobs(blob_image, b), blobs))
     
@@ -72,24 +115,31 @@ class BlobFeatureExtractor:
 
 class BlobExtractionManager:
   def __init__(self, frames_dir, dest_dir):
-    
     self.frames = glob.glob(os.path.join(frames_dir, '*'))
     self.dest_dir = dest_dir
     self.extractor = BlobFeatureExtractor()
-    
-  def compute_blobs(self, file):
-    return self.extractor.extract(file)
-    
+    self.white_parameters = white_params()
+    self.black_parameters = black_params()
+      
   def extract(self, line):
     index, file = line
 
     if index % 50 == 0:
       print(datetime.now(), index)
+      
+    image = skimage.io.imread(file)
+    image = np.array(skimage.exposure.equalize_adapthist(image, clip_limit=0.03) * 255, dtype=np.uint8)
+
+    white_blob_image = self.extractor.extract(image, self.white_parameters)
+    black_blob_image = self.extractor.extract(image, self.black_parameters)
     
-    blob_image = self.compute_blobs(file)    
     _, id = os.path.split(file)
+    
+    id, ext = id.split('.')
     dest_file = os.path.join(self.dest_dir, id)
-    skimage.io.imsave(dest_file, blob_image)
+    
+    skimage.io.imsave(dest_file + '_white.' + ext, white_blob_image)
+    skimage.io.imsave(dest_file + '_black.' + ext, black_blob_image)
     
   def split_in_batches(self, n_split):
     image_files = self.frames
@@ -103,9 +153,14 @@ class BlobExtractionManager:
   def run_extraction_batch(self, files):
     list(map(self.extract, enumerate(files)))
     
-  def run_extraction(self):    
+  def run_extraction(self, parallel=False):
     processes = 4
-    batches_of_files = self.split_in_batches(processes)
-    print('Split files in', len(batches_of_files), 'batches')
-    pool = mp.ProcessingPool(processes)
-    pool.map(lambda batch_files: self.run_extraction_batch(batch_files), batches_of_files)
+    if parallel:
+      batches_of_files = self.split_in_batches(processes)
+      print('Split files in', len(batches_of_files), 'batches')
+      pool = mp.ProcessingPool(processes)
+      pool.map(lambda batch_files: self.run_extraction_batch(batch_files), batches_of_files)
+      
+    else:
+      batches_of_files = self.split_in_batches(processes)
+      list(map(lambda batch_files: self.run_extraction_batch(batch_files), batches_of_files))
